8316f7f139227150a95c2b23aa0f039f4286862a
Pierrick Pluchon <pluchonp@proximity.bbdo.fr>
2018-10-31 12:06:21 +0100

    Utils : refacto

--- src/App/utils/Utils.js
+++ src/App/utils/Utils.js
@@ -1,5 +1,21 @@
+/*
+*   NETWORK
+*/
+
 export const URLify = str => {
   str = str.replace(/'/gm, '')
   str = str.replace(/ /gm, '-')
   return str
+}
+
+export const Status = response => {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
+}
+
+export const ToJson = response => {
+  return response.json()
 }
